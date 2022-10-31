8242490064651658720e17db7ed09ddd3ce1f38d
Akari SC <86619805+aksac3@users.noreply.github.com>
2022-10-31 13:51:10 -0600

    new addition

---
+++ automat.js
@@ -0,0 +1,12 @@
+const puppeteer = require('puppeteer');
+
+async function uploadCvs() {
+  const browser = await puppeteer.launch({
+    headless: false,
+  });
+  const page = await browser.newPage();
+  await page.goto('https://https://todoist.com/app/project/2301311976');
+  const element = await page.$('input[type=file]');
+  await element.uploadFile('./ToDoList.csv');
+}
+uploadCvs();
